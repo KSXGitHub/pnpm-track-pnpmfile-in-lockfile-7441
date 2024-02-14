467fb481a7dbc04c1f258d6c916af87e220481c1
khai96_ <hvksmr1996@gmail.com>
2024-02-15 01:52:31 +0700

    ignore scripts

--- reproduce.bash
+++ reproduce.bash
@@ -3,7 +3,7 @@ set -o errexit -o pipefail -o nounset
 cd "$(dirname "$0")"
 
 echo 'Reproducing...' >&2
-pretty-exec -- pnpm install
+pretty-exec -- pnpm install --ignore-scripts
 pretty-exec -- bash -c 'lsd node_modules/.pnpm/cpu-features*'
 pretty-exec -- cp would-be-pnpmfile.cjs .pnpmfile.cjs
 pretty-exec -- bash -c 'lsd node_modules/.pnpm/cpu-features*'
