582f399cc8e1d372441ee4e7647f6d16dedc85cf
khai96_ <hvksmr1996@gmail.com>
2024-02-15 01:54:38 +0700

    change inspect command

--- reproduce.bash
+++ reproduce.bash
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 
 echo 'Reproducing...' >&2
 pretty-exec -- pd install --ignore-scripts
-pretty-exec -- bash -c 'lsd node_modules/.pnpm/cpu-features*'
+pretty-exec -- bash -c 'ls node_modules/.pnpm | grep cpu-features'
 pretty-exec -- cp would-be-pnpmfile.cjs .pnpmfile.cjs
 pretty-exec -- pd install --ignore-scripts
-pretty-exec -- bash -c 'lsd node_modules/.pnpm/cpu-features*'
+pretty-exec -- bash -c 'ls node_modules/.pnpm | grep cpu-features'
