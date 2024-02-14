bef5414282d20d479f46d2f164834c71268aa6d0
khai96_ <hvksmr1996@gmail.com>
2024-02-15 01:53:15 +0700

    install again

--- reproduce.bash
+++ reproduce.bash
@@ -6,4 +6,5 @@ echo 'Reproducing...' >&2
 pretty-exec -- pd install --ignore-scripts
 pretty-exec -- bash -c 'lsd node_modules/.pnpm/cpu-features*'
 pretty-exec -- cp would-be-pnpmfile.cjs .pnpmfile.cjs
+pretty-exec -- pd install --ignore-scripts
 pretty-exec -- bash -c 'lsd node_modules/.pnpm/cpu-features*'
